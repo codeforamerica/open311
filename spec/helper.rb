6ff91309bdae98381ca3c7a7a190cfa707f50433
Erik Michaels-Ober <sferik@gmail.com>
2014-11-01 20:45:08 +0100

    Enforce code coverage minimums

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,7 +1,12 @@
-unless ENV['CI']
+if RUBY_VERSION >= '1.9'
   require 'simplecov'
-  SimpleCov.start
+
+  SimpleCov.start do
+    add_filter '/spec/'
+    minimum_coverage(97.44)
+  end
 end
+
 require 'open311'
 require 'rspec'
 require 'webmock/rspec'
